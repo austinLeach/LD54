707957a93dd9fe8fcbc8f38a77dd15ea0b3cb608
Austin Leach <austinleach850@gmail.com>
2023-09-29 21:33:27 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+    public float gravity = -9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // recieves input from InputManager.cs and applies it to character controller
+    public void ProcessMove(Vector2 input) 
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        // playerVelocity += gravity * Time.deltaTime;
+    }
+}
